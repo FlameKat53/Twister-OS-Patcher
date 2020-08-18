2896e106c3bd740dfaa795234c64f28a612d2f21
FlameKat53 <flame9804@yahoo.com>
2020-08-18 13:40:17 -0500

    i left debug in there :p

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 echo Updating TwisterOS to the latest version
 read -p "The updater will now update TwisterOS. When ready, press [Enter] to try to update, or, press [CTRL+C] to close the updater..."
 cd ~/updater/
-#wget https://twisteros.com/Patches/checkversion.sh
+wget https://twisteros.com/Patches/checkversion.sh
 . checkversion.sh
 FILE=~/updater/latest.txt
 if [ -f "$FILE" ]; then
