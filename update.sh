17368f4110db8a91f1ef380240f9c15651b7a2a2
FlameKat53 <flame9804@yahoo.com>
2020-08-18 16:38:27 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,12 +4,16 @@ read -p "This application will now attempt to update Twister OS. When ready, pre
 cd ~/updater/
 wget https://twisteros.com/Patches/checkversion.sh
 . checkversion.sh
-FILE=~/updater/latest.txt
-if [ -f "$FILE" ]; then
-    rm latest.txt
+FILE1=~/updater/checkversion.sh
+if [ -f "$FILE1" ]; then
+    rm -f checkversion.sh
+fi
+FILE2=~/updater/latest.txt
+if [ -f "$FILE2" ]; then
+    rm -f latest.txt
 fi
 wget https://twisteros.com/Patches/latest.txt
-if [ -f "$FILE" ]; then
+if [ -f "$FILE2" ]; then
 VERSIONLATEST=$(cat latest.txt)
 	rm -f *patchinstall.sh
 	checkVersion
