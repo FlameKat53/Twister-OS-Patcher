a07a9a5e31e467a05a84a536544efe8c1079f8a3
FlameKat53 <flame9804@yahoo.com>
2020-08-18 14:17:59 -0500

    add checks

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,8 @@
 echo 'Uninstalling Twister OS Updater by FlameKat53 and MobileGamesMotionYT...'
 cd ~/
+if [ -d "~/updater/" ]; then
 rm -r ~/updater/
+fi
+if [ -f "~/.local/share/applications/updater.desktop" ]; then
 rm ~/.local/share/applications/updater.desktop
+fi
