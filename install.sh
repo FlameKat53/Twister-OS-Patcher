8ef3bc50aa2e3fef3932fd7410e77f019b8aa105
FlameKat53 <flame9804@yahoo.com>
2020-08-17 22:08:09 -0500

    that should work

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 echo 'Installing TwisterOS Patcher by FlameKat53 and MobileGamesMotionYT...'
-mkdir ~/updater/
-cd ~/updater/ 
-mv 
-mv updater.desktop /usr/share/applications
-
+git clone --depth=1 https://github.com/FlameKat53/TwisterOS-Updater.git
+cd TwisterOS-Updater/
+mv updater.png ~/updater/updater.png
+mv update.sh ~/updater/update.sh
+mv updater.desktop ~/.local/share/applications/updater.desktop
+cd ../
+rm -r TwisterOS-Updater/
