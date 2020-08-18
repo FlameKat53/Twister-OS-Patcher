aac5dbf88dadd362cc0c4affd926e9afca6de4fe
FlameKat53 <flame9804@yahoo.com>
2020-08-17 22:27:42 -0500

    whoops!

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo 'Installing TwisterOS Patcher by FlameKat53 and MobileGamesMotionYT...'
 wget https://github.com/FlameKat53/TwisterOS-Updater/archive/master.zip
 unzip master.zip
 cd TwisterOS-Updater-master/
+mkdir ~/updater/
 mv updater.png ~/updater/updater.png
 mv update.sh ~/updater/update.sh
 mv updater.desktop ~/.local/share/applications/updater.desktop
