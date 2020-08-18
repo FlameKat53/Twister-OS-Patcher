c9ff7cfa3cbb0f6fb95f100ca5a75c604eb3eb08
FlameKat53 <flame9804@yahoo.com>
2020-08-18 14:57:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ echo 'Installing Twister OS Patcher by FlameKat53 and MobileGamesMotionYT...'
 wget https://github.com/FlameKat53/Twister-OS-Updater/archive/master.zip
 unzip master.zip
 cd Twister-OS-Updater-master/
-mkdir ~/updater/
-mkdir ~/updater/icons/
+mkdir -p ~/updater/
+mkdir -p ~/updater/icons/
 mv twister-update.png ~/updater/icons/twister-update.png
 mv twister-update-blue.png ~/updater/icons/twister-update-blue.png
 mv twister-update-dark.png ~/updater/icons/twister-update-dark.png
