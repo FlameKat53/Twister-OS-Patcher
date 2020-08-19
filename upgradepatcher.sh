8e00dd61dc337a2916e9cffa9894011067243e01
mobilegmYT <zhengm58@gmail.com>
2020-08-19 17:06:02 -0600

    ~/

--- upgradepatcher.sh
+++ upgradepatcher.sh
@@ -2,13 +2,13 @@
 cp ~/Desktop/patcher.desktop ~/Pictures/patcher.desktop
 
 #Uninstalls and reinstalls the patcher
-bash /home/pi/patcher/uninstall.sh
+bash ~/patcher/uninstall.sh
 wget https://raw.githubusercontent.com/FlameKat53/Twister-OS-Patcher/master/install.sh && chmod +x ./install.sh && ./install.sh && rm install.sh
 
 #Remove new desktop files
 rm ~/Desktop/patcher.desktop
-rm /home/pi/.local/share/applications/patcher.desktop
+rm ~/.local/share/applications/patcher.desktop
 
 #Replace with old one
-cp ~/Pictures/patcher.desktop /home/pi/.local/share/applications/patcher.desktop
+cp ~/Pictures/patcher.desktop ~/.local/share/applications/patcher.desktop
 mv ~/Pictures/patcher.desktop ~/Desktop/patcher.desktop
