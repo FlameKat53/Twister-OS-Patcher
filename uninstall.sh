4a9420b4c5251c30fac3b2f8a44fd836ba95678b
FlameKat53 <flame9804@yahoo.com>
2020-08-19 17:12:33 -0500

    ~/

--- uninstall.sh
+++ uninstall.sh
@@ -1,16 +1,16 @@
 echo 'Uninstalling Twister OS Patcher by FlameKat53 and MobileGamesMotionYT...'
 echo 'Thanks for using me! You can install me again at "https://bit.ly/patchtwist"'
-cd /home/pi/
+cd ~
 
 #Checks to see if these files exist and replace or remove them
-if [ -d /home/pi/patcher/ ]; then
-  rm -r /home/pi/patcher/
+if [ -d ~/patcher/ ]; then
+  rm -r ~/patcher/
 fi
-if [ -f /home/pi/.local/share/applications/patcher.desktop ]; then
-  rm /home/pi/.local/share/applications/patcher.desktop
+if [ -f ~/.local/share/applications/patcher.desktop ]; then
+  rm ~/.local/share/applications/patcher.desktop
 fi
-if [ -f /home/pi/Desktop/patcher.desktop ]; then
-  rm /home/pi/Desktop/patcher.desktop
+if [ -f ~/Desktop/patcher.desktop ]; then
+  rm ~/Desktop/patcher.desktop
 fi
 sudo rm -f /usr/local/bin/twistpatch
 sudo rm -f /usr/local/bin/twistpatch-uninstall
