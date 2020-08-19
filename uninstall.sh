87fac6008083e666220e94d73011219ec0400389
FlameKat53 <flame9804@yahoo.com>
2020-08-18 20:19:58 -0500

    indentation is nice

--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,9 @@ echo 'Uninstalling Twister OS Patcher by FlameKat53 and MobileGamesMotionYT...'
 echo 'Thanks for using me! You can install me again at "https://bit.ly/patchtwist"'
 cd ~/
 if [ -d ~/patcher/ ]; then
-rm -r ~/patcher/
+  rm -r ~/patcher/
 fi
 if [ -f ~/.local/share/applications/patcher.desktop ]; then
-rm ~/.local/share/applications/patcher.desktop
-rm ~/Desktop/patcher.desktop
+  rm ~/.local/share/applications/patcher.desktop
+  rm ~/Desktop/patcher.desktop
 fi
