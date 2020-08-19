29a137d00cdd64f7698f0c42fba34c0b72a5d4ec
mobilegmYT <zhengm58@gmail.com>
2020-08-18 18:58:30 -0600

    change to patchr

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@ echo 'Installing Twister OS Patcher by FlameKat53 and MobileGamesMotionYT...'
 wget https://github.com/FlameKat53/Twister-OS-Patcher/archive/master.zip
 unzip master.zip
 cd Twister-OS-Patcher-master/
-mkdir -p ~/updater/icons/
-mv twister-update.png ~/updater/icons/twister-update.png
-mv twister-update-blue.png ~/updater/icons/twister-update-blue.png
-mv twister-update-dark.png ~/updater/icons/twister-update-dark.png
-mv update.sh ~/updater/update.sh
-mv uninstall.sh ~/updater/uninstall.sh
-mv updater.desktop ~/.local/share/applications/updater.desktop
-cp ~/.local/share/applications/updater.desktop ~/Desktop
+mkdir -p ~/patcher/icons/
+mv twister-update.png ~/patcher/icons/twister-update.png
+mv twister-update-blue.png ~/patcher/icons/twister-update-blue.png
+mv twister-update-dark.png ~/patcher/icons/twister-update-dark.png
+mv update.sh ~/patcher/update.sh
+mv uninstall.sh ~/patcher/uninstall.sh
+mv patcher.desktop ~/.local/share/applications/patcher.desktop
+cp ~/.local/share/applications/patcher.desktop ~/Desktop
 cd ../
 rm -r Twister-OS-Patcher-master/
 rm master.zip
-cd ~/updater/
+cd ~/patcher/
 chmod +x *
--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Checking for and installing available Twister OS patches..."
 read -p "This application will now attempt to update Twister OS. When ready, press [Enter] to update, or press [CTRL+C] to close the updater..."
-cd ~/updater/
+cd ~/patcher/
 wget https://twisteros.com/Patches/checkversion.sh
 . checkversion.sh
-FILE1=~/updater/checkversion.sh
+FILE1=~/patcher/checkversion.sh
 if [ -f "$FILE1" ]; then
     rm -f checkversion.sh
 fi
-FILE2=~/updater/latest.txt
+FILE2=~/patcher/latest.txt
 if [ -f "$FILE2" ]; then
     rm -f latest.txt
 fi
