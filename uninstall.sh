7cbf2919f8eb12099b2ca8804ec04c323352c990
FlameKat53 <flame9804@yahoo.com>
2020-08-18 19:51:27 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
-echo 'Uninstalling Twister OS Updater by FlameKat53 and MobileGamesMotionYT...'
+echo 'Uninstalling Twister OS Patcher by FlameKat53 and MobileGamesMotionYT...'
 echo 'Thanks for using me! You can install me again at "https://bit.ly/twistpatch"'
 cd ~/
 if [ -d ~/updater/ ]; then
