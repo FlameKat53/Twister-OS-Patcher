a1bb61a2079829a2033a3194268f327d257adefb
mobilegmYT <zhengm58@gmail.com>
2020-08-18 18:59:05 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,9 @@
 echo 'Uninstalling Twister OS Patcher by FlameKat53 and MobileGamesMotionYT...'
 echo 'Thanks for using me! You can install me again at "https://bit.ly/patchtwist"'
 cd ~/
-if [ -d ~/updater/ ]; then
-rm -r ~/updater/
+if [ -d ~/patcher/ ]; then
+rm -r ~/patcher/
 fi
-if [ -f ~/.local/share/applications/updater.desktop ]; then
-rm ~/.local/share/applications/updater.desktop
+if [ -f ~/.local/share/applications/patcher.desktop ]; then
+rm ~/.local/share/applications/patcher.desktop
 fi
