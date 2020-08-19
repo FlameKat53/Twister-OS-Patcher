25d54142cf457c9bf7e832c75cd6cd07e31a2efa
FlameKat53 <flame9804@yahoo.com>
2020-08-19 14:14:07 -0500

    bleh

--- install.sh
+++ install.sh
@@ -36,10 +36,10 @@ if [ ! -f "/usr/local/bin/twistpatch-update" ]; then
 fi
 
 #Install commands
-#sudo ln -s /home/pi/updater/patch.sh /usr/local/bin/twistpatch
-#sudo ln -s /home/pi/updater/uninstall.sh /usr/local/bin/twistpatch-uninstall
-#sudo ln -s /home/pi/updater/upgradepatcher.sh /usr/local/bin/twistpatch-update
-#source ~/.bashrc
+sudo ln -s /home/pi/updater/patch.sh /usr/local/bin/twistpatch
+sudo ln -s /home/pi/updater/uninstall.sh /usr/local/bin/twistpatch-uninstall
+sudo ln -s /home/pi/updater/upgradepatcher.sh /usr/local/bin/twistpatch-update
+source ~/.bashrc
 
 #Remove useless folders and make all files executable
 cd ../
