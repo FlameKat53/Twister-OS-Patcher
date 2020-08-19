b192b1eed9346d9d0a65988d3cbaad1951ad8f5b
FlameKat53 <flame9804@yahoo.com>
2020-08-19 14:24:49 -0500

    oops!

--- checkforupdates.sh
+++ checkforupdates.sh
@@ -2,5 +2,5 @@ VERSIONCURRENT=$(twistver)
 WEBVERSION=$(curl https://twisteros.com/Patches/latest.txt)
 
 if [ "$VERSIONCURRENT" != "$WEBVERSION" ]; then
-    notify-send --urgency=normal --app-name=Twister-OS-Patcher --icon=svn-update New update available. Run Twister OS Patcher to update.
+    notify-send --urgency=normal --app-name=Twister-OS-Patcher --icon=svn-update "New update available. Run Twister OS Patcher to update."
 fi
