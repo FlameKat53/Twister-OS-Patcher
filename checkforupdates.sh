fe6acf6ba26c4d88ffff2686db7cda30bd704216
FlameKat53 <flame9804@yahoo.com>
2020-08-20 11:25:41 -0500

    Add files via upload

--- checkforupdates.sh
+++ checkforupdates.sh
@@ -4,5 +4,5 @@ VERSIONCURRENT=$(twistver)
 WEBVERSION=$(curl https://twisteros.com/Patches/latest.txt)
 
 if [ "$VERSIONCURRENT" != "$WEBVERSION" ]; then
-    notify-send.sh --urgency=normal --app-name=twistpatch --icon=/home/pi/patcher/icons/arrow-white.png --action=Update:twistpatch $WEBVERSION 'is now avaliable, click "Update" to update'
+    ./notify-send.sh --urgency=normal --expire-time=20000 --app-name=twistpatch --icon=/home/pi/patcher/icons/arrow-white.png --action=Update:twistpatch $WEBVERSION 'is now avaliable, click "Update" to update'
 fi
