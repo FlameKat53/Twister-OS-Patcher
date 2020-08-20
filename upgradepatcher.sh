bed2a500e248ceb05ee2ad01ac6357d39e036d23
mobilegmYT <zhengm58@gmail.com>
2020-08-20 16:00:23 -0600

    Update upgradepatcher.sh

--- upgradepatcher.sh
+++ upgradepatcher.sh
@@ -1,4 +1,20 @@
-#!/bin/bash
+#Copy .desktop file and cron
+cp ~/Desktop/patcher.desktop ~/Pictures/patcher.desktop
+cp ~/Desktop/patcher.desktop ~/Pictures/patcher.desktop
+
 #Uninstalls and reinstalls the patcher
 bash ~/patcher/uninstall.sh
+bash ~/patcher/uninstall.sh
 wget https://raw.githubusercontent.com/FlameKat53/Twister-OS-Patcher/master/install.sh && chmod +x ./install.sh && ./install.sh && rm install.sh
+
+#Remove new desktop files
+rm ~/Desktop/patcher.desktop
+rm ~/.local/share/applications/patcher.desktop
+rm ~/Desktop/patcher.desktop
+rm ~/.local/share/applications/patcher.desktop
+
+#Replace with old one
+cp ~/Pictures/patcher.desktop ~/.local/share/applications/patcher.desktop
+mv ~/Pictures/patcher.desktop ~/Desktop/patcher.desktop 
+cp ~/Pictures/patcher.desktop ~/.local/share/applications/patcher.desktop
+mv ~/Pictures/patcher.desktop ~/Desktop/patcher.desktop
