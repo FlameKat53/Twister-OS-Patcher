81d3ad70f107ae6c9d012e5cb40d27af9bee9aeb
FlameKat53 <flame9804@yahoo.com>
2020-08-20 21:25:48 -0500

    Update checkforupdates.sh

--- checkforupdates.sh
+++ checkforupdates.sh
@@ -2,7 +2,5 @@
 cd ~/patcher/
 VERSIONCURRENT=$(twistver)
 WEBVERSION=$(curl https://twisteros.com/Patches/latest.txt)
+./notify-send.sh --urgency=normal --expire-time=20000 --app-name=twistpatch --icon=/home/pi/patcher/icons/arrow-white.png --action=Update:twistpatch "$WEBVERSION" 'is now avaliable, click [Update] to upgrade now!'
 
-if [ "$VERSIONCURRENT" != "$WEBVERSION" ]; then
-    ./notify-send.sh --urgency=normal --expire-time=20000 --app-name=twistpatch --icon=/home/pi/patcher/icons/arrow-white.png --action=Update:twistpatch 'Twister OS update' $WEBVERSION 'is now avaliable, click [Update] to upgrade now!'
-fi
