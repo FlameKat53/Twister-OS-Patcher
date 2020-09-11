e0123ff9d32390717e5de7c420d9fe09c989519a
mobilegmYT <zhengm58@gmail.com>
2020-09-11 16:55:38 -0600

    Update checkforupdates.sh

--- checkforupdates.sh
+++ checkforupdates.sh
@@ -3,5 +3,5 @@ cd /home/pi/patcher/
 VERSIONCURRENT=$(twistver)
 WEBVERSION=$(curl https://twisteros.com/Patches/latest.txt)
 if [ "$VERSIONCURRENT" != "$WEBVERSION" ]; then
-    ./notify-send.sh --urgency=normal --expire-time=20000 --app-name=TwistPatch --icon=/home/pi/patcher/icons/arrow-white.png --action=Update:/home/pi/patcher/patch.sh "A new Twister OS update is now avaliable, click [Update] to update to patch" $WEBVERSION
+    ./notify-send.sh --urgency=normal --expire-time=20000 --app-name=TwistPatch --icon=/home/pi/patcher/icons/arrow-white.png --action=Update:/home/pi/patcher/patch.sh "A new Twister OS update is now avaliable, click [Update] to patch to release" $WEBVERSION
 fi
