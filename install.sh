ddd139a56bdfaff090714dcc392277b92f25551e
FlameKat53 <flame9804@yahoo.com>
2020-09-13 18:44:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ echo Checking for tkinter: $problem
 if [ "" == "$problem" ]; then
 	sudo apt-get install python3-tk
 fi
-sudo apt-get install python3-pil python3-pil.imagetk python3-wget
+sudo apt-get install python3-pil python3-pil.imagetk python3-wget python3-schedule
 sudo python3 c_desktop.py $USER
 sudo chmod +x ${path}/src/start.sh
