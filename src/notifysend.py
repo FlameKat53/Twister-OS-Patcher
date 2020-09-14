440e9bc897307459964e4537cb5e58df8c24560e
FlameKat53 <flame9804@yahoo.com>
2020-09-13 19:05:31 -0500

    Update notifysend.py

--- src/notifysend.py
+++ src/notifysend.py
@@ -5,7 +5,7 @@ import time
 
 def job():
     if rs.twistver != rs.webversion:
-        os.system('WEBVERSION=$(curl https://twisteros.com/Patches/latest.txt) && /home/pi/patcher/notify-send.sh --urgency=normal --expire-time=20000 --app-name=TwistPatch --icon=/home/pi/patcher/icons/src/icons/update.png --action=Update:/home/pi/patcher/src/start.sh "A new Twister OS update is now avaliable, click [Update] to update to" "$WEBVERSION"')
+        os.system('WEBVERSION=$(curl https://twisteros.com/Patches/latest.txt) && /home/pi/patcher/notify-send.sh --urgency=normal --expire-time=20000 --app-name=TwistPatch --icon=/home/pi/patcher/src/icons/update.png --action=Update:/home/pi/patcher/src/start.sh "A new Twister OS update is now avaliable, click [Update] to update to" "$WEBVERSION"')
 
 schedule.every().saturday.at("12:00").do(job)
 
