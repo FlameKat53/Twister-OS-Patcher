42a3f84896a3c1f7cf26d6b1b02a98f5fec3ac6a
FlameKat53 <flame9804@yahoo.com>
2020-09-14 11:51:02 -0500

    Beta 2, new install.sh and a few minor things?

--- src/resources.py
+++ src/resources.py
@@ -14,12 +14,12 @@ if os.path.exists('/home/pi/patcher/src/pat.config'):
 else:
 	print("Creating config...")
 	config['DEFAULT'] = {'color_mode': '0',
-	'version': 'Beta 1'}
+	'version': 'Beta 2'}
 	with open('/home/pi/patcher/src/pat.config', 'w') as configfile:
 		config.write(configfile)
 
 ### update stuff
-app_version = "Version Beta 1\n"
+app_version = "Version Beta 2\n"
 def get_app_version():
 	return app_version
 
