38a89d2912c0237561fcdddebf0492ff038d4478
FlameKat53 <flame9804@yahoo.com>
2020-09-14 11:45:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo 'Installing Twister OS Patcher by FlameKat53, Jack47, and MobileGamesMotionYT...'
+
 problem=$(dpkg -s python3-tk|grep installed)
 path=$(pwd)
 echo $path
@@ -7,5 +9,13 @@ if [ "" == "$problem" ]; then
 	sudo apt-get install python3-tk
 fi
 sudo apt-get install python3-pil python3-pil.imagetk python3-wget python3-schedule
+#Get archive from github and unzip it
+wget https://github.com/FlameKat53/Twister-OS-Patcher/archive/py.zip
+unzip py.zip
+rm py.zip
+#wget https://github.com/FlameKat53/Twister-OS-Patcher/archive/master.zip
+#unzip master.zip
+#rm master.zip
+mv Twister-OS-Patcher-master/ ~/patcher/
 sudo python3 c_desktop.py $USER
-sudo chmod +x ${path}/src/start.sh
+sudo chmod +x /home/pi/patcher/src/start.sh
