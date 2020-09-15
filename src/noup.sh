91d51ccfc625b11374eb315759d184c6810e6339
FlameKat53 <flame9804@yahoo.com>
2020-09-15 13:49:36 -0500

    fix crash

--- src/noup.sh
+++ src/noup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -f /home/pi/patcher/src/checkversion.py
 wget https://twisteros.com/Patches/checkversion.py
-sudo python3 /home/pi/patcher/src/main.py ${HOME} 0> log.txt
+sudo python3 /home/pi/patcher/src/main.py ${HOME} 0 > log.txt
