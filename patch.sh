93334dbf01852641cb1f3417bf3aba8516a17bcf
FlameKat53 <flame9804@yahoo.com>
2020-09-14 23:21:08 -0500

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,31 +1,4 @@
 #!/bin/bash
-echo "Checking for and installing available Twister OS patches... Press Ctrl-C to cancel"
-echo "Please wait while we get the latest patch files."
-sleep 5s
-
-#Download the latest patchversion.sh and run it to determine your current version
-cd ~/patcher/
-wget https://twisteros.com/Patches/checkversion.sh
-. checkversion.sh
-FILE1=~/patcher/checkversion.sh
-if [ -f "$FILE1" ]; then
-    rm -f checkversion.sh
-fi
-FILE2=~/patcher/latest.txt
-if [ -f "$FILE2" ]; then
-    rm -f latest.txt
-fi
-wget https://twisteros.com/Patches/latest.txt
-if [ -f "$FILE2" ]; then
-VERSIONLATEST=$(cat latest.txt)
-	rm -f *patchinstall.sh
-	checkVersion
-	clear
-	echo "You are running the most current version, " $VERSIONLATEST ". Exiting..."
-	sleep 2.5s
-
-else
-	clear
-	echo "You seem to be offline! Exiting..."
-	sleep 1.5s
-fi
+rm -f /home/pi/patcher/src/checkversion.py
+wget https://twisteros.com/Patches/checkversion.py
+sudo python3 /home/pi/patcher/src/main.py ${HOME} $1> log.txt
