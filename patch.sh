57159fe8141da93d785155e86781b212ad94b047
FlameKat53 <flame9804@yahoo.com>
2020-09-15 14:01:22 -0500

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -2,4 +2,4 @@
 cd /home/pi/patcher/src/
 rm -f /home/pi/patcher/src/checkversion.py
 wget https://twisteros.com/Patches/checkversion.py
-sudo python3 /home/pi/patcher/src/main.py ${HOME} 1> log.txt
+sudo python3 /home/pi/patcher/src/main.py ${HOME} 1 > log.txt
