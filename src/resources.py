e72580dad91c334ef9c78abcc806327bfc6ef910
FlameKat53 <flame9804@yahoo.com>
2020-09-16 14:19:41 -0500

    Update resources.py

--- src/resources.py
+++ src/resources.py
@@ -14,12 +14,12 @@ if os.path.exists('/home/pi/patcher/src/pat.config'):
 else:
 	print("Creating config...")
 	config['DEFAULT'] = {'color_mode': '0',
-	'version': 'Omega 0.1'}
+	'version': 'Omega 0.1.1'}
 	with open('/home/pi/patcher/src/pat.config', 'w') as configfile:
 		config.write(configfile)
 
 ### update stuff
-app_version = "Version Omega 0.1\n"
+app_version = "Version Omega 0.1.1\n"
 def get_app_version():
 	return app_version
 
