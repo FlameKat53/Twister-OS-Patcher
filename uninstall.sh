7bc3c2073f9552fad165c78abae55a0a1c2b65a0
FlameKat53 <flame9804@yahoo.com>
2020-09-16 14:46:57 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,15 +4,13 @@ echo 'Thanks for using me! You can install me again at "https://bit.ly/patchtwis
 cd ~
 
 #Checks to see if these files exist and replace or remove them
-if [ -d ~/patcher/ ]; then
-  sudo rm -r ~/patcher/
+  sudo rm -r /home/pi/patcher/
   # we need sudo for the pycache files
+if [ -f /home/pi/.local/share/applications/patcher.desktop ]; then
+  sudo rm /home/pi/.local/share/applications/patcher.desktop
 fi
-if [ -f ~/.local/share/applications/patcher.desktop ]; then
-  sudo rm ~/.local/share/applications/patcher.desktop
-fi
-if [ -f ~/Desktop/patcher.desktop ]; then
-  sudo rm ~/Desktop/patcher.desktop
+if [ -f /home/pi/Desktop/patcher.desktop ]; then
+  sudo rm /home/pi/Desktop/patcher.desktop
 fi
 sudo rm -f /usr/local/bin/twistpatch
 sudo rm -f /usr/local/bin/twistpatch-update
