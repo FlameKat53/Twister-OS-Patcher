c9bea91f0a1cdd759cb4ecffd164b61b46a29f70
FlameKat53 <flame9804@yahoo.com>
2020-09-19 20:21:34 -0500

    Update upgradepatcher.sh

--- upgradepatcher.sh
+++ upgradepatcher.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Uninstalls and reinstalls the patcher
 bash ~/patcher/uninstall.sh
 wget https://raw.githubusercontent.com/FlameKat53/Twister-OS-Patcher/py/install.sh && chmod +x ./install.sh && ./install.sh && rm install.sh
