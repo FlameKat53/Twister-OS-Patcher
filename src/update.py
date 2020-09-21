98e49a0b2f2378a3e71c15a0d06b7c426c6dd623
FlameKat53 <flame9804@yahoo.com>
2020-09-21 12:48:31 -0500

    Update update.py

--- src/update.py
+++ src/update.py
@@ -14,9 +14,6 @@ path = os.path.dirname(os.path.realpath(__file__))
 def delete_oldfiles():
 	os.system('rm -f /home/pi/patcher/src/*patchinstall.sh')
 
-def download_patch():
-	cv.update()
-
 def update_twist():
 	if rs.twistver == rs.webversion:
 		msb.showinfo(title="TwistPatch", message="There is no update available!")
@@ -24,7 +21,7 @@ def update_twist():
 		answer = msb.askyesno(title="TwistPatch", message="This will update Twister OS it could take a while, do you wish to proceed?")
 		if answer == True:
 			delete_oldfiles()
-			download_patch()
+			cv.update()
 
 def is_connected(hostname):
   try:
@@ -39,21 +36,6 @@ def is_connected(hostname):
 def check_online():
 	url = "https://raw.githubusercontent.com/FlameKat53/Twister-OS-Patcher/py/src/resources.py"
 	if is_connected("1.1.1.1"):
-		#with urllib.request.urlopen(url) as f:
-		#	xcontent = f.read().decode('utf-8')
-		#	xcontent = xcontent.splitlines()
-		#	xversion = ""
-		#	for line in xcontent:
-		#		if "app_version =" in line:
-		#			xversion=line
-		#	if rs.app_version[:-1] in xversion:
-		#		print("It works bc it's same version!")
-		#	else:
-		#		answer = msb.askyesno(title="TwistPatch", message='A patcher update is avaliable.\nWould you like to update the patcher?')
-		#		if answer == True:
-		#			update_pat()
-		#		print(rs.app_version)
-		#		print(xversion)
 		print("Network is connected")
 		return True
 	else:
