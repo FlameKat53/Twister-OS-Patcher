6f95b93c3d4f809b3d8007b8ec48885ef281f1e3
mobilegmYT <44128563+mobilegmYT@users.noreply.github.com>
2020-09-27 19:02:35 -0600

    Update c_desktop.py

--- c_desktop.py
+++ c_desktop.py
@@ -3,17 +3,13 @@ import sys
 import os
 import subprocess as sp
 from os.path import expanduser
-from /home/pi/patcher/src/resources.py import get_app_version
-
-patcherversion = str(get_app_version())
-
 user = sys.argv[1]
 home_path = expanduser("~%s" % user)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 print(dir_path)
-f_content = "[Desktop Entry]\nName=Twister OS Patcher\nComment=Updates Twister OS\nExec="+dir_path+"/src/start.sh 0\nIcon="+dir_path+"/src/icon.png\nCategories=System;\nVersion="+patcherversion+"\nType=Application\nTerminal=false\nStartupNotify=true"
+f_content = "[Desktop Entry]\nName=Twister OS Patcher\nComment=Updates Twister OS\nExec="+dir_path+"/src/start.sh 0\nIcon="+dir_path+"/src/icon.png\nCategories=System;\nVersion=0.1.2\nType=Application\nTerminal=false\nStartupNotify=true"
 print(f_content)
 
 x_dir = "/home/pi/.local/share/applications/patcher.desktop"
