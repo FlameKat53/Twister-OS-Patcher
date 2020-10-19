8d7c2f930e8e31b5090987c6ab4239178b97e4ab
mobilegmYT <44128563+mobilegmYT@users.noreply.github.com>
2020-10-19 15:48:18 -0600

    Update upgradepatcher.sh

--- upgradepatcher.sh
+++ upgradepatcher.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #Uninstalls and reinstalls the patcher
 bash ~/patcher/uninstall.sh
-wget https://raw.githubusercontent.com/FlameKat53/Twister-OS-Patcher/master/install.sh && chmod +x ./install.sh && ./install.sh && rm install.sh
+cd ~
+wget https://raw.githubusercontent.com/FlameKat53/Twister-OS-Patcher/master/install.sh
+chmod +x ./install.sh
+./install.sh
+cd ~
+rm install.sh
