f8db26694286ad798226bba1e44cd88730acc865
FlameKat53 <flame9804@yahoo.com>
2020-11-02 15:31:26 -0600

    Update checkversion.py

--- src/checkversion.py
+++ src/checkversion.py
@@ -21,42 +21,42 @@ def update():
 		if os.path.exists('/usr/bin/xscreensaver') == False:
 			answer = msb.askyesno(title="TwistPatch", message='You appear to be running Twister OS version 1.0\nIs this correct?')
 			if answer == True:
-				msb.showinfo(title="TwistPatch", message='Downloading 1.1.0 Patch in the background...\nPress OK to begin downloading.')
+				#msb.showinfo(title="TwistPatch", message='Downloading 1.1.0 Patch in the background...\nPress OK to begin downloading.')
 				wget.download('https://twisteros.com/Patches/TwisterOSv1-1Patch.zip', out='/home/pi/patcher/src/patch.zip')
 				other()
 
 		if os.path.exists('/usr/bin/com.github.libredeb.lightpad') == False:
 			answer =  msb.askyesno(title="TwistPatch", message='You appear to be running Twister OS version 1.1\nIs this correct?')
 			if answer == True:
-				msb.showinfo(title="TwistPatch", message='Downloading 1.2.0 Patch in the background...\nPress OK to begin downloading.')
+				#msb.showinfo(title="TwistPatch", message='Downloading 1.2.0 Patch in the background...\nPress OK to begin downloading.')
 				wget.download('https://twisteros.com/Patches/TwisterOSv1-2Patch.zip', out='/home/pi/patcher/src/patch.zip')
 				other()
 
 		if os.path.exists('/home/pi/WebApps/Discord/discord.sh') == False:
 			answer = msb.askyesno(title="TwistPatch", message='You appear to be running Twister OS version 1.2\nIs this correct?')
 			if answer == True:
-				msb.showinfo(title="TwistPatch", message='Downloading 1.3.0 Patch in the background...\nPress OK to begin downloading.')
+				#msb.showinfo(title="TwistPatch", message='Downloading 1.3.0 Patch in the background...\nPress OK to begin downloading.')
 				wget.download('https://twisteros.com/Patches/TwisterOSv1-3Patch.zip', out='/home/pi/patcher/src/patch.zip')
 				other()
 
 		if os.path.exists('/usr/share/ThemeSwitcher/Raspbian95/splash.png') == False:
 			answer = msb.askyesno(title="TwistPatch", message='You appear to be running Twister OS version 1.4.0\nIs this correct?')
 			if answer == True:
-				msb.showinfo(title="TwistPatch", message='Downloading 1.4.1 Patch in the background...\nPress OK to begin downloading.')
+				#msb.showinfo(title="TwistPatch", message='Downloading 1.4.1 Patch in the background...\nPress OK to begin downloading.')
 				wget.download('https://archive.org/download/twister-osv-1-4-1-patch/TwisterOSv1-4-1Patch.zip', out='/home/pi/patcher/src/patch.zip')
 				other()
 
 		if os.path.exists('/usr/share/ThemeSwitcher/RaspbianXP/splash.png') == False:
 			answer = msb.askyesno(title="TwistPatch", message='You appear to be running Twister OS version 1.4.1\nIs this correct?')
 			if answer == True:
-				msb.showinfo(title="TwistPatch", message='Downloading 1.5.0 Patch in the background...\nPress OK to begin downloading.')
+				#msb.showinfo(title="TwistPatch", message='Downloading 1.5.0 Patch in the background...\nPress OK to begin downloading.')
 				wget.download('https://twisteros.com/Patches/TwisterOS1-5Patch.zip', out='/home/pi/patcher/src/patch.zip')
 				other()
 
 		if os.path.exists('/usr/local/bin/twistver') == False:
 			answer = msb.askyesno(title="TwistPatch", message='You appear to be running Twister OS version 1.5.0 or 1.5.1\nIs this correct?')
 			if answer == True:
-				msb.showinfo(title="TwistPatch", message='Downloading 1.5.2 Patch in the background...\nPress OK to begin downloading.')
+				#msb.showinfo(title="TwistPatch", message='Downloading 1.5.2 Patch in the background...\nPress OK to begin downloading.')
 				wget.download('https://twisteros.com/Patches/TwisterOSv1-5-2Patch.zip', out='/home/pi/patcher/src/patch.zip')
 				other()
 
